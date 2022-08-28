fb504b114df33170c2941fc11af41f85cd78ea83
suvrick <suvrick@outlook.com>
2022-08-28 22:02:55 +0700

    changes logic for game

--- main.go
+++ main.go
@@ -2,67 +2,70 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"time"
 
-	"github.com/suvrick/go-kiss-core/frame"
-	"github.com/suvrick/go-kiss-core/leb128"
 	"github.com/suvrick/go-kiss-core/ws"
 )
 
 var urls = []string{
-	"https://bottle2.itsrealgames.com/www/fs.html?5&apiUrl=https%3A%2F%2Fapi.fotostrana.ru%2Fapifs.php&apiId=bottle&userId=100046693&viewerId=100046693&isAppUser=1&isAppWidgetUser=0&sessionKey=5d121ddedbef9721fc0fc02d33a2011a6938773f38a853&authKey=dd52b12107363624100e77b8b5160b02&apiSettings=743&silentBilling=1&lang=ru&fromServiceBlock=0&ls=0&pos=2&is_global=1&from_id=left.menu.service&from=left.menu.service&hasNotifications=1&_v=1&isOfferWallEnabled=0&appManage=0&connId=1563080077&ourIp=0&lc_name=&fs_api=https://st.fotocdn.net/swf/api/__v1344942768.fs_api.swf&log=0&swfobject=https://st.fotocdn.net/js/__v1368780425.swfobject2.js&fsapi=https://st.fotocdn.net/app/app/js/__v1540476017.fsapi.js&xdm_e=https://fotostrana.ru&xdm_c=default0&xdm_p=1",
-	"https://bottle2.itsrealgames.com/www/fs.html?5&apiUrl=https%3A%2F%2Fapi.fotostrana.ru%2Fapifs.php&apiId=bottle&userId=100088538&viewerId=100088538&isAppUser=1&isAppWidgetUser=0&sessionKey=5d5a438a024349b54f24de4e2900ed26a89089f36d4edd&authKey=0f649b5a99bcd94ee913839afe100e75&apiSettings=743&silentBilling=1&lang=ru&fromServiceBlock=1&ls=1&pos=1&isFavlb=1&is_global=1&from_id=left.menu.service&from=left.menu.service&hasNotifications=1&_v=1&isOfferWallEnabled=0&appManage=0&connId=1563888983&ourIp=0&lc_name=&fs_api=https://st.fotocdn.net/swf/api/__v1344942768.fs_api.swf&log=0&swfobject=https://st.fotocdn.net/js/__v1368780425.swfobject2.js&fsapi=https://st.fotocdn.net/app/app/js/__v1540476017.fsapi.js&xdm_e=https://fotostrana.ru&xdm_c=default0&xdm_p=1",
-	"https://bottle2.itsrealgames.com/www/fs.html?5&apiUrl=https%3A%2F%2Fapi.fotostrana.ru%2Fapifs.php&apiId=bottle&userId=100114300&viewerId=100114300&isAppUser=1&isAppWidgetUser=0&sessionKey=5d1153d086c9b167f5aa239744f92226d35da8283b02c7&authKey=cdb75b921e5797b6a34d21dc1188003c&apiSettings=743&silentBilling=1&lang=ru&fromServiceBlock=1&ls=1&pos=1&isFavlb=1&is_global=1&from_id=left.menu.service&from=left.menu.service&hasNotifications=1&_v=1&isOfferWallEnabled=0&appManage=0&connId=1563054359&ourIp=0&lc_name=&fs_api=https://st.fotocdn.net/swf/api/__v1344942768.fs_api.swf&log=0&swfobject=https://st.fotocdn.net/js/__v1368780425_1_4.swfobject2.js&fsapi=https://st.fotocdn.net/app/app/js/__v1540476017_1_4.fsapi.js&xdm_e=https://fotostrana.ru&xdm_c=default0&xdm_p=1",
-	"https://bottle2.itsrealgames.com/www/fs.html?5&apiUrl=https%3A%2F%2Fapi.fotostrana.ru%2Fapifs.php&apiId=bottle&userId=100215745&viewerId=100215745&isAppUser=1&isAppWidgetUser=0&sessionKey=5de7c2e21b149f78d195e60e048e42b25c4906f346a238&authKey=0ed6709b750a47e518e46e0e16e5c265&apiSettings=743&silentBilling=1&lang=ru&fromServiceBlock=1&ls=10&pos=1&isFavlb=1&is_global=1&from_id=left.menu.service&from=left.menu.service&hasNotifications=1&_v=1&isOfferWallEnabled=0&appManage=0&connId=1570857178&ourIp=0&lc_name=&fs_api=https://st.fotocdn.net/swf/api/__v1344942768.fs_api.swf&log=0&swfobject=https://st.fotocdn.net/js/__v1368780425.swfobject2.js&fsapi=https://st.fotocdn.net/app/app/js/__v1540476017.fsapi.js&xdm_e=https://fotostrana.ru&xdm_c=default0&xdm_p=1",
-	"https://bottle2.itsrealgames.com/www/fs.html?5&apiUrl=https%3A%2F%2Fapi.fotostrana.ru%2Fapifs.php&apiId=bottle&userId=100221042&viewerId=100221042&isAppUser=1&isAppWidgetUser=0&sessionKey=5d56e7867437f91f4dcabba819421670e3c3575d7c5440&authKey=c5f9af3ccf00d7922067e0e3a398e56d&apiSettings=743&silentBilling=1&lang=ru&fromServiceBlock=1&ls=0&pos=1&isFavlb=1&is_global=1&from_id=left.menu.service&from=left.menu.service&hasNotifications=1&_v=1&isOfferWallEnabled=0&appManage=0&connId=1563079622&ourIp=0&lc_name=&fs_api=https://st.fotocdn.net/swf/api/__v1344942768.fs_api.swf&log=0&swfobject=https://st.fotocdn.net/js/__v1368780425.swfobject2.js&fsapi=https://st.fotocdn.net/app/app/js/__v1540476017.fsapi.js&xdm_e=https://fotostrana.ru&xdm_c=default0&xdm_p=1",
-	"view-source:https://bottle2.itsrealgames.com/www/fs.html?5&apiUrl=https%3A%2F%2Fapi.fotostrana.ru%2Fapifs.php&apiId=bottle&userId=103786125&viewerId=103786125&isAppUser=1&isAppWidgetUser=0&sessionKey=5da6d124c7f823d753f582f7a5b39fbd85919e9d2b5cb6&authKey=65ea9ab0e227437a66d73a010bff42c2&apiSettings=743&silentBilling=1&lang=ru&forceInstall=1&from=app.popup&from_id=app.popup&hasNotifications=0&_v=1&isOfferWallEnabled=0&appManage=0&connId=1569557906&ourIp=0&lc_name=&fs_api=https://st.fotocdn.net/swf/api/__v1344942768.fs_api.swf&log=0&swfobject=https://st.fotocdn.net/js/__v1368780425_1_4.swfobject2.js&fsapi=https://st.fotocdn.net/app/app/js/__v1540476017_1_4.fsapi.js&xdm_e=https://fotostrana.ru&xdm_c=default0&xdm_p=1#api=fs&packageName=bottlePackage&config=config_release.xml&protocol=https:&locale=RU&international=false&locale_url=../resources/locale/EN_All.lp?158&width=1000&height=690&sprites_version=83&useApiType=fs&",
-	"view-source:https://bottle2.itsrealgames.com/www/fs.html?5&apiUrl=https%3A%2F%2Fapi.fotostrana.ru%2Fapifs.php&apiId=bottle&userId=103786202&viewerId=103786202&isAppUser=1&isAppWidgetUser=0&sessionKey=5d063a76c0ea63fd48b4d06f8cc85e45122d9abe8bb4f0&authKey=b508c5dbc13ee88cf28affbb76d5fe34&apiSettings=743&silentBilling=1&lang=ru&forceInstall=1&from=app.popup&from_id=app.popup&hasNotifications=0&_v=1&isOfferWallEnabled=0&appManage=0&connId=1569558150&ourIp=0&lc_name=&fs_api=https://st.fotocdn.net/swf/api/__v1344942768.fs_api.swf&log=0&swfobject=https://st.fotocdn.net/js/__v1368780425.swfobject2.js&fsapi=https://st.fotocdn.net/app/app/js/__v1540476017.fsapi.js&xdm_e=https://fotostrana.ru&xdm_c=default0&xdm_p=1#api=fs&packageName=bottlePackage&config=config_release.xml&protocol=https:&locale=RU&international=false&locale_url=../resources/locale/EN_All.lp?158&width=1000&height=690&sprites_version=83&useApiType=fs&",
-	"view-source:https://bottle2.itsrealgames.com/www/fs.html?5&apiUrl=https%3A%2F%2Fapi.fotostrana.ru%2Fapifs.php&apiId=bottle&userId=103786258&viewerId=103786258&isAppUser=1&isAppWidgetUser=0&sessionKey=5d09db98a83f25ff3885114f725c651022ee76138454ff&authKey=dc93c8e0c365ca792cf1198ab71c73e7&apiSettings=743&silentBilling=1&lang=ru&forceInstall=1&from=app.popup&from_id=app.popup&hasNotifications=0&_v=1&isOfferWallEnabled=0&appManage=0&connId=1569558375&ourIp=0&lc_name=&fs_api=https://st.fotocdn.net/swf/api/__v1344942768.fs_api.swf&log=0&swfobject=https://st.fotocdn.net/js/__v1368780425.swfobject2.js&fsapi=https://st.fotocdn.net/app/app/js/__v1540476017.fsapi.js&xdm_e=https://fotostrana.ru&xdm_c=default0&xdm_p=1#api=fs&packageName=bottlePackage&config=config_release.xml&protocol=https:&locale=RU&international=false&locale_url=../resources/locale/EN_All.lp?158&width=1000&height=690&sprites_version=83&useApiType=fs&",
+	"",
+	"invalid frame",
+	"view-source:https://bottle2.itsrealgames.com/www/fs.html?5&apiUrl=https%3A%2F%2Fapi.fotostrana.ru%2Fapifs.php&apiId=bottle&userId=104015872&viewerId=104015872&isAppUser=1&isAppWidgetUser=0&sessionKey=5db68d0bc9be09339317d785fd88ec42ae7976d721bf8e&authKey=3d6ac3df40e40ffc4243b650efae46f5&apiSettings=743&silentBilling=1&lang=ru&forceInstall=1&from=app.popup&from_id=app.popup&hasNotifications=0&_v=1&isOfferWallEnabled=0&appManage=0&connId=1570189957&ourIp=0&lc_name=&fs_api=https://st.fotocdn.net/swf/api/__v1344942768.fs_api.swf&log=0&swfobject=https://st.fotocdn.net/js/__v1368780425.swfobject2.js&fsapi=https://st.fotocdn.net/app/app/js/__v1540476017.fsapi.js&xdm_e=https://fotostrana.ru&xdm_c=default0&xdm_p=1#api=fs&packageName=bottlePackage&config=config_release.xml&protocol=https:&locale=RU&international=false&locale_url=../resources/locale/EN_All.lp?161&width=1000&height=690&sprites_version=87&useApiType=fs&",
+	"https://bottle2.itsrealgames.com/www/sa.html?time=1571552893425&&userId=114941701&sessionKey=67f5e4f7a90144c5eba1b91694132904&authKey=33513e2ce85cabfd6ec59d827aa28cea&net_type=32&useApiType=sa&email=tsd_8326@mail.ru&locale=RU",
+	"https://m.inspin.me/build/v431/?api=ok&5&sig=13c32cf30efbedcbfd7afa689f9fa5b6&mob=true&refplace=user_apps_portlet&session_key=-s-6622x0r.b2d-yvr6022bQSS26241xUv3433cvvq4c232NwOZ376bTuxd62-4TxN41178Msr1a63fMXr45827NWo277c0TUq8b265Ne&session_secret_key=7f16a539d8d56e2fca91be30ac68c7e3&auth_sig=d04204aa3632ab2dcf9e6f2b41084b57&api_server=https%3A%2F%2Fapi.ok.ru%2F&payment_server=https%3A%2F%2Fmpay.ok.ru&lang=ru&application_key=CBADLOPFABABABABA&mob_platform=androidweb&logged_user_id=952642558996&type=ok",
+	"https://bottle2.itsrealgames.com/www/sa.html?time=1561991107710&&userId=1000937&sessionKey=6c92d6b72649d11dc6a436777c8f3fd&authKey=3109ee5271d550293cf9b2fb494556f1&net_type=32&useApiType=sa",
+	"https://bottle2.itsrealgames.com/mobile/build/v1593/?social_api=mm&type=mm&record_first_session=1&6=&is_app_user=1&session_key=95ac81be3e64f73fd56e4b12e004c9d0&vid=9914552860808032409&oid=9914552860808032409&app_id=543574&authentication_key=27861583a5de8bd7c1aa5955dfe757b8&session_expire=1623244207&ext_perm=photos%2Cfriends%2Cevents%2Cguestbook%2Cmessages%2Cnotifications%2Cstream%2Cemails%2Cpayments&sig=2add120d8f235755ee8b34801798f775&window_id=CometName_659cc22f549d0d25dc769cb39daa105f&referer_type=left_menu&version=1593",
+	"https://bottle2.itsrealgames.com/mobile/build/v1593/?social_api=mm&type=mm&record_first_session=1&6=&is_app_user=1&session_key=f53f650cd57b6bc75da0b65af0d0c028&vid=13402962412699287699&oid=13402962412699287699&app_id=543574&authentication_key=e1de7d6b1b9a18e124331d1a8e7a6709&session_expire=1623248257&ext_perm=notifications%2Cemails%2Cpayments&sig=d38fca257b4651d5fc2bbc3e2531842f&window_id=CometName_74be9f9e99659ab7f65e85f2a31d3d3b&referer_type=left_menu&version=1593",
+	"https://bottle2.itsrealgames.com/mobile/build/v1593/?social_api=mm&type=mm&record_first_session=1&6=&is_app_user=1&session_key=f53f650cd57b6bc75da0b65af0d0c028&vid=13402962412699287699&oid=13402962412699287699&app_id=543574&authentication_key=e1de7d6b1b9a18e124331d1a8e7a6709&session_expire=1623248257&ext_perm=notifications%2Cemails%2Cpayments&sig=d38fca257b4651d5fc2bbc3e2531842f&window_id=CometName_74be9f9e99659ab7f65e85f2a31d3d3b&referer_type=left_menu&version=1593",
 }
+
+// 38 48
+// ['14408', 41, 6, '207f27da4e9113369c402a86b7c033e7', true, '', 0, 0, 0, '', 0, '', 2, '']
+
+/*
+29 00 04 06 c8 70 29 06 20 32 30 37 66 32 37 64 61 34 65 39 31 31 33333639633430326138366237633033336537
+32 00 04 06 c8 70 29 06 20 32 30 37 66 32 37 64 61 34 65 39 31 31 33 33 36 39 63 34 30 32 61 38 36 62 37 63 30 33 33 65 37 01 00 00 00 00 00 00 00 02 00
+*/
 
 func main() {
 
-	f := frame.NewFrameDefault()
+	// f := frame.NewFrameDefault()
 
-	id, r, _ := f.GetValue(urls[0])
+	// _, r, _ := f.GetValue(urls[2])
 
-	bb := make([]byte, 0)
-	for _, v := range r {
-		b := leb128.Write(v)
-		bb = append(bb, b...)
-	}
+	// // r := []interface{}{
+	// // 	len, index, type, device, uint64(14408), 41, 6, "207f27da4e9113369c402a86b7c033e7",
+	// // }
 
-	bb = append(leb128.Write(4), bb...)
-	bb = append(leb128.Write(0), bb...)
-	l := leb128.Write(len(bb))
-	bb = append(l, bb...)
+	wait := make(chan struct{}, 1)
 
 	s := ws.NewSocket(ws.GetDefaultSocketConfig())
 	s.SetErrorHandler(func(err error) {
 		log.Println("socket error")
 		log.Println(err.Error())
+		s.Close()
 	})
-
 	s.SetOpenHandler(func() {
 		log.Println("socket open")
 	})
-
 	s.SetCloseHandler(func(rule byte, msg string) {
 		log.Println("socket close")
+		wait <- struct{}{}
 	})
-
-	s.SetReadHandler(func(reader io.Reader) {
-		var b = make([]byte, 512)
-		reader.Read(b)
-		log.Println(b)
+	s.SetReadHandler(func(packType ws.PacketServerType, structure interface{}) {
+		fmt.Printf("%+v\n", structure)
 	})
-
-	fmt.Printf("%d, %v, %v", id, r, bb)
 
 	s.Go()
-	s.Send(bb)
 
-	<-time.After(1 * time.Minute)
+	login := ws.PCLogin{
+		ID:      uint64(14408),
+		SocType: 41,
+		Device2: 6,
+		Token:   "207f27da4e9113369c402a86b7c033e7",
+	}
+
+	s.SendPacket(4, &login)
+
+	<-wait
 }
--- ws/socket.go
+++ ws/socket.go
@@ -1,12 +1,41 @@
 package ws
 
 import (
+	"errors"
 	"io"
+	"reflect"
 	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
+
+type PCLogin struct {
+	ID      uint64
+	SocType uint16
+	Device2 byte
+	Token   string
+}
+
+type PSBalance struct {
+}
+
+type PSBonus struct {
+	CanCollect byte
+	Day        byte
+}
+
+type PSLogin struct {
+	Result  byte
+	UserID  uint64
+	Balance int32
+}
+
+type PSUserInfo struct {
+	Result  byte
+	UserID  uint64
+	Balance int32
+}
 
 // GameSock ...
 type Socket struct {
@@ -14,8 +43,8 @@ type Socket struct {
 
 	rule_close byte
 	client     *websocket.Conn
-
-	wg sync.WaitGroup
+	msgID      int
+	wg         sync.WaitGroup
 
 	send_chan  chan []byte
 	read_chan  chan []byte
@@ -23,7 +52,7 @@ type Socket struct {
 
 	openHandle  func()
 	closeHandle func(byte, string)
-	readHandle  func(io.Reader)
+	readHandle  func(PacketServerType, interface{})
 	errorHandle func(error)
 }
 
@@ -33,6 +62,32 @@ const (
 	ERROR_READ_CLOSE    = 0x02
 	ERROR_SEND_CLOSE    = 0x03
 	ERROR_TIMEOUT_CLOSE = 0x04
+)
+
+type PacketClientType uint16
+
+const (
+	LOGIN     PacketClientType = 4
+	GET_BONUS PacketClientType = 61
+	BUY       PacketClientType = 6
+)
+
+type PacketServerType uint16
+
+const (
+	LOGIN_SERVER PacketServerType = 4
+	INFO         PacketServerType = 5
+	BALANCE      PacketServerType = 7
+	BONUS        PacketServerType = 17
+)
+
+var (
+	ErrTimeoutTheGame           = errors.New("time in the game success")
+	ErrConnectionNot101         = errors.New("websocket connection fail")
+	ErrConnectionFail           = errors.New("connection fail")
+	ErrProxyConnectionFail      = errors.New("proxy connection fail")
+	ErrCloseConnectionByTimeout = errors.New("close connection by timeout")
+	ErrMarshalClientPacket      = errors.New("error marshal client packet")
 )
 
 var closed_rules = map[byte]string{
@@ -62,7 +117,7 @@ func (socket *Socket) SetCloseHandler(handler func(rule byte, msg string)) {
 	socket.closeHandle = handler
 }
 
-func (socket *Socket) SetReadHandler(handler func(reader io.Reader)) {
+func (socket *Socket) SetReadHandler(handler func(packetType PacketServerType, structure interface{})) {
 	socket.readHandle = handler
 }
 
@@ -90,6 +145,42 @@ func (socket *Socket) Send(packet []byte) {
 
 		socket.send_chan <- packet
 	}()
+}
+
+func (socket *Socket) SendPacket(packType uint16, payload interface{}) {
+
+	pack, err := Marshal(payload)
+
+	if err != nil {
+		if socket.errorHandle != nil {
+			socket.errorHandle(ErrMarshalClientPacket)
+		}
+		return
+	}
+
+	data := make([]byte, 0)
+
+	data = AppendInt(data, int64(socket.msgID)) // message ID
+
+	data = AppendUint(data, uint64(packType)) // packet type
+
+	data = AppendUint(data, uint64(6)) //device
+
+	data = append(data, pack...)
+
+	data_len := make([]byte, 0)
+
+	data_len = AppendInt(data_len, int64(len(data))) // packet len
+
+	data_len = append(data_len, data...)
+
+	// socket.Logger.Printf("[SendPacket] >> %x\n", pack)
+
+	// socket.Logger.Printf("[SendPacket2] >> %x\n", data_len)
+
+	socket.Send(data_len)
+
+	socket.msgID = socket.msgID + 1
 }
 
 func (socket *Socket) Close() {
@@ -209,7 +300,46 @@ func (socket *Socket) read() {
 		}
 
 		if socket.readHandle != nil {
-			socket.readHandle(reader)
+
+			ReadInt(reader, 32)
+			ReadInt(reader, 32)
+			t, _ := ReadUint(reader, 16)
+
+			packetType := PacketServerType(t)
+
+			socket.Logger.Printf("server type: %d\n", packetType)
+
+			switch packetType {
+			case LOGIN_SERVER:
+				login := PSLogin{}
+				if err := Unmarshal(reader, &login); err != nil {
+					if socket.errorHandle != nil {
+						socket.errorHandle(err)
+					}
+				} else {
+					socket.readHandle(packetType, login)
+				}
+
+			case BALANCE:
+				balance := PSBalance{}
+				if err := Unmarshal(reader, &balance); err != nil {
+					if socket.errorHandle != nil {
+						socket.errorHandle(err)
+					}
+				} else {
+					socket.readHandle(packetType, balance)
+				}
+			case BONUS:
+				bonus := PSBonus{}
+				if err := Unmarshal(reader, &bonus); err != nil {
+					if socket.errorHandle != nil {
+						socket.errorHandle(err)
+					}
+				} else {
+					socket.readHandle(packetType, bonus)
+				}
+			}
+
 		}
 	}
 }
@@ -235,5 +365,301 @@ func (socket *Socket) close_chan() {
 func (socket *Socket) setClosedRule(rule byte) {
 	if socket.rule_close == 255 {
 		socket.rule_close = rule
+	}
+}
+
+func Unmarshal(reader io.Reader, s interface{}) error {
+
+	var _err error = nil
+	var _uint uint64 = 0
+	var _int int64 = 0
+
+	structure := reflect.ValueOf(s)
+
+	numfield := reflect.ValueOf(s).Elem().NumField()
+
+	for x := 0; x < numfield; x++ {
+
+		field := structure.Elem().Field(x)
+
+		switch reflect.ValueOf(s).Elem().Field(x).Kind() {
+		case reflect.Int8:
+			_int, _err = ReadInt(reader, 8)
+			if _err != nil {
+				return _err
+			}
+
+			field.SetInt(_int)
+		case reflect.Uint8:
+			_uint, _err = ReadUint(reader, 8)
+			if _err != nil {
+				return _err
+			}
+
+			field.SetUint(_uint)
+		case reflect.Int16:
+			_int, _err = ReadInt(reader, 16)
+			if _err != nil {
+				return _err
+			}
+
+			field.SetInt(_int)
+		case reflect.Uint16:
+			_uint, _err = ReadUint(reader, 32)
+			if _err != nil {
+				return _err
+			}
+
+			field.SetUint(_uint)
+		case reflect.Int:
+			_int, _err = ReadInt(reader, 32)
+			if _err != nil {
+				return _err
+			}
+
+			field.SetInt(_int)
+		case reflect.Uint:
+			_uint, _err = ReadUint(reader, 32)
+			if _err != nil {
+				return _err
+			}
+
+			field.SetUint(_uint)
+		case reflect.Int32:
+			_int, _err = ReadInt(reader, 32)
+			if _err != nil {
+				return _err
+			}
+
+			field.SetInt(_int)
+		case reflect.Uint32:
+			_uint, _err = ReadUint(reader, 32)
+			if _err != nil {
+				return _err
+			}
+
+			field.SetUint(_uint)
+		case reflect.Int64:
+			_int, _err = ReadInt(reader, 64)
+			if _err != nil {
+				return _err
+			}
+
+			field.SetInt(_int)
+		case reflect.Uint64:
+			_uint, _err = ReadUint(reader, 64)
+			if _err != nil {
+				return _err
+			}
+
+			field.SetUint(_uint)
+		case reflect.String:
+			_int, _err = ReadInt(reader, 32)
+			if _err != nil {
+				return _err
+			}
+
+			str := make([]byte, _int)
+
+			reader.Read(str)
+
+			field.SetString(string(str))
+		default:
+			_err = ErrMarshalClientPacket
+			return _err
+		}
+	}
+
+	return nil
+}
+
+func Marshal(s interface{}) ([]byte, error) {
+
+	result := make([]byte, 0)
+
+	var err error = nil
+
+	structure := reflect.ValueOf(s)
+
+	numfield := reflect.ValueOf(s).Elem().NumField()
+
+	for x := 0; x < numfield; x++ {
+
+		if err != nil {
+			return nil, err
+		}
+
+		field := structure.Elem().Field(x)
+
+		switch reflect.ValueOf(s).Elem().Field(x).Kind() {
+		case reflect.Int8:
+			if i, ok := field.Interface().(int8); ok {
+				result = AppendInt(result, int64(i))
+			} else {
+				err = ErrMarshalClientPacket
+			}
+		case reflect.Uint8:
+			if i, ok := field.Interface().(uint8); ok {
+				result = AppendUint(result, uint64(i))
+			} else {
+				err = ErrMarshalClientPacket
+			}
+		case reflect.Int16:
+			if i, ok := field.Interface().(int16); ok {
+				result = AppendInt(result, int64(i))
+			} else {
+				err = ErrMarshalClientPacket
+			}
+		case reflect.Uint16:
+			if i, ok := field.Interface().(uint16); ok {
+				result = AppendUint(result, uint64(i))
+			} else {
+				err = ErrMarshalClientPacket
+			}
+		case reflect.Int:
+			if i, ok := field.Interface().(int); ok {
+				result = AppendInt(result, int64(i))
+			} else {
+				err = ErrMarshalClientPacket
+			}
+		case reflect.Uint:
+			if i, ok := field.Interface().(uint); ok {
+				result = AppendUint(result, uint64(i))
+			} else {
+				err = ErrMarshalClientPacket
+			}
+		case reflect.Int32:
+			if i, ok := field.Interface().(int32); ok {
+				result = AppendInt(result, int64(i))
+			} else {
+				err = ErrMarshalClientPacket
+			}
+		case reflect.Uint32:
+			if i, ok := field.Interface().(uint32); ok {
+				result = AppendUint(result, uint64(i))
+			} else {
+				err = ErrMarshalClientPacket
+			}
+		case reflect.Int64:
+			if i, ok := field.Interface().(int64); ok {
+				result = AppendInt(result, int64(i))
+			} else {
+				err = ErrMarshalClientPacket
+			}
+		case reflect.Uint64:
+			if i, ok := field.Interface().(uint64); ok {
+				result = AppendUint(result, uint64(i))
+			} else {
+				err = ErrMarshalClientPacket
+			}
+		case reflect.String:
+			if i, ok := field.Interface().(string); ok {
+				str := []byte(i)
+				result = AppendInt(result, int64(len(str)))
+				result = append(result, str...)
+			} else {
+				err = ErrMarshalClientPacket
+			}
+		default:
+			err = ErrMarshalClientPacket
+		}
+	}
+
+	return result, nil
+}
+
+// AppendUleb128 appends v to b using unsigned LEB128 encoding.
+func AppendUint(b []byte, v uint64) []byte {
+	for {
+		//берём 7 бит
+		// 	13 -> 0 0 0 0  1 1 0 1
+		// 127 -> 0 1 1 1  1 1 1 1
+		c := uint8(v & 0x7f)
+		//сдвигаем на 7 бит
+		v >>= 7
+		if v != 0 {
+			// 1 0 0 0 0 0 0 0
+			c |= 0x80
+		}
+		b = append(b, c)
+		if c&0x80 == 0 {
+			break
+		}
+	}
+	return b
+}
+
+// AppendSleb128 appends v to b using signed LEB128 encoding.
+func AppendInt(b []byte, v int64) []byte {
+	for {
+		c := uint8(v & 0x7f) // берем первых 7 бит
+		s := uint8(v & 0x40)
+		v >>= 7 // сдвигайем на 7 бит вправо
+		if (v != -1 || s == 0) && (v != 0 || s != 0) {
+			// если вошли сюда то
+			c |= 0x80 // дописываем 8 бит
+		}
+		b = append(b, c)
+		if c&0x80 == 0 {
+			break
+		}
+	}
+	return b
+}
+
+func ReadUint(r io.Reader, n uint) (uint64, error) {
+	if n > 64 {
+		panic(errors.New("leb128: n must <= 64"))
+	}
+	p := make([]byte, 1)
+	var res uint64
+	var shift uint
+	for {
+		_, err := io.ReadFull(r, p)
+		if err != nil {
+			return 0, err
+		}
+		b := uint64(p[0])
+		switch {
+		case b < 1<<7 && b < 1<<n:
+			res += (1 << shift) * b
+			return res, nil
+		case b >= 1<<7 && n > 7:
+			res += (1 << shift) * (b - 1<<7)
+			shift += 7
+			n -= 7
+		default:
+			return 0, errors.New("leb128: invalid uint")
+		}
+	}
+}
+
+func ReadInt(r io.Reader, n uint) (int64, error) {
+	if n > 64 {
+		panic(errors.New("leb128: n must <= 64"))
+	}
+	p := make([]byte, 1)
+	var res int64
+	var shift uint
+	for {
+		_, err := io.ReadFull(r, p)
+		if err != nil {
+			return 0, err
+		}
+		b := int64(p[0])
+		switch {
+		case b < 1<<6 && uint64(b) < uint64(1<<(n-1)):
+			res += (1 << shift) * b
+			return res, nil
+		case b >= 1<<6 && b < 1<<7 && uint64(b)+1<<(n-1) >= 1<<7:
+			res += (1 << shift) * (b - 1<<7)
+			return res, nil
+		case b >= 1<<7 && n > 7:
+			res += (1 << shift) * (b - 1<<7)
+			shift += 7
+			n -= 7
+		default:
+			return 0, errors.New("leb128: invalid int")
+		}
 	}
 }
